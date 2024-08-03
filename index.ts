803ee56f344626b8d3b189e9d465cd3614367df0
katherinekhine <khaingkatherine@gmail.com>
2024-08-03 21:18:29 +0630

    update index.ts

--- index.ts
+++ index.ts
@@ -4,3 +4,7 @@ import { Motorcycle } from "./MotorCycle";
 const toyota = new Car("Toyota", "Corolla", 4, 1990);
 toyota.startEngine();
 toyota.displayCarInfo();
+
+const honda = new Motorcycle("Honda", "Civic", "Honda");
+honda.startEngine();
+honda.displayMotorcycleInfo();
